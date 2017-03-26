6323f5e80d552cfdf00b773ea14a207c824a3379
kimhyunsuk <hskim@taihoinst.com>
2017-03-26 14:35:41 +0900

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -5,15 +5,15 @@ var fs = require('fs');
 
 /* GET home page. */
 router.get('/', function (req, res) {
-    res.render('index', { title: 'Express' });
-    /*fs.readFile('index.html', function (error, data) {
+    //res.render('index', { title: 'Express' });
+    fs.readFile('index.html', function (error, data) {
         if (error) {
             console.log(error);
         } else {
             res.writeHead(200, { 'Content-Type': 'text/html' });
             res.end(data);
         }
-    });*/
+    });
 });
 
 module.exports = router;
