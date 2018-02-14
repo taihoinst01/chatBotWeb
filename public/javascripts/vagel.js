c67047597e5879611416ede1ce0b960069d14cd0
songhj <songhj@SONGHJ-PC>
2018-02-14 18:44:52 +0900

    commit

--- public/javascripts/vagel.js
+++ public/javascripts/vagel.js
@@ -27,12 +27,12 @@ function printMsg(flag,msg){
 
         //$(".wc-textbox").html('<input type="text" class="wc-shellinput" value="'+msg+'" placeholder="Type your message...">')
 
-        $("input[type=text]").val(msg);
-        $(".wc-console").addClass("has-text");
-        $(".wc-console.has-text .wc-send svg").css("fill", "#3a96dd");
-        var e = jQuery.Event("keydown", { keyCode: 110 });
-        $("input[type=text]").trigger(e);
-        $(".wc-send").trigger("click");
+        //$("input[type=text]").val(msg);
+        //$(".wc-console").addClass("has-text");
+        //$(".wc-console.has-text .wc-send svg").css("fill", "#3a96dd");
+        //var e = jQuery.Event("keydown", { keyCode: 110 });
+        //$("input[type=text]").trigger(e);
+        //$(".wc-send").trigger("click");
         //var e = jQuery.Event("keydown", { keyCode: 13 });
         //$(this).trigger(e);
 	}
