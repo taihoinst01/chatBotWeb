412bb0da28557a1d8ee8237fa5c9cfd117f1e354
songhj <songhj@SONGHJ-PC>
2018-02-18 17:25:56 +0900

    test commit

--- public/javascripts/vagel.js
+++ public/javascripts/vagel.js
@@ -34,7 +34,7 @@ function printMsg(flag,msg){
         //$(".wc-send").trigger("click");
         //$(".wc-message-group-content").append('<div class="wc-message-wrapper list"><div class="wc-message wc-message-from-me"><div class="wc-message-content"><svg class="wc-message-callout"><path class="point-left" d="m0,6 l6 6 v-12 z"></path><path class="point-right" d="m6,6 l-6 6 v-12 z"></path></svg><div><span class="format-plain"><span><!-- react-text: 93 -->호빵도시락<!-- /react-text --><br></span></span><!-- react-empty: 95 --></div></div></div><div class="wc-message-from wc-message-from-me"><span><!-- react-text: 150 -->userid<!-- /react-text --></span></div></div>');
         //var e = jQuery.Event("keyup", { keyCode: 32 });
-        var e = $.Event("keydown");
+        var e = $.Event("keyup");
         e.which = 32;
         e.keyCode = 32;
         $("input[type=text]").trigger(e);
