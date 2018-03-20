e64255453b894fc21fa56d5b14c90081574b3f44
taihoinst01 <gjlim@taihoinst.com>
2018-03-20 08:31:54 +0000

    Update vagel.js

--- public/javascripts/vagel.js
+++ public/javascripts/vagel.js
@@ -3,7 +3,8 @@ $(document).ready(function() {
 });
 
 //apikey 획득 정보 	  : https://azure.microsoft.com/ko-kr/try/cognitive-services/?api=speech-api
-var bingClientTTS = new BingSpeech.TTSClient("150528dddf6947509ec84ecd5e6584a7");
+//var bingClientTTS = new BingSpeech.TTSClient("150528dddf6947509ec84ecd5e6584a7");
+var bingClientTTS = new BingSpeech.TTSClient("ccce5a983d5d4e8fa932dc521ec8df4e");
 
 function lfn_speakTTS(text, speaker){
 	bingClientTTS.multipleXHR = true;
